4b68ebb4abadd0f97d31ee04d330168e66bb01e9
yashashvisri <138010917+yashashvisri@users.noreply.github.com>
2026-06-10 00:25:43 +0530

    opensource_llm_models

---
+++ EmbeddedModels/hf_query.py
@@ -0,0 +1,9 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+
+embedding = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+result = embedding.embed_query("Delhi is the capital of India")
+
+print(str(result))
